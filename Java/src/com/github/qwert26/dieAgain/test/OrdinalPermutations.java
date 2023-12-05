2c56a3081ca9a67e8a9eeeab4d1b3b3fa7196ed5
Christian Schürhoff <qwert268862@gmail.com>
2023-12-05 16:33:13 +0100

    Create OrdinalPermutations.java

---
+++ Java/src/com/github/qwert26/dieAgain/test/OrdinalPermutations.java
@@ -0,0 +1,25 @@
+package com.github.qwert26.dieAgain.test;
+
+import java.util.Random;
+
+import com.github.qwert26.dieAgain.util.StandardTest;
+
+/**
+ * 
+ */
+public class OrdinalPermutations implements ITest {
+
+	/**
+	 * 
+	 */
+	public OrdinalPermutations() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void runTestOn(Random rng, StandardTest... parameters) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
