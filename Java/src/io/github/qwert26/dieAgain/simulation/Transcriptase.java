dd91611b89d86c06055f5d1b699c8458e0810935
Christian Schürhoff <qwert268862@gmail.com>
2024-12-02 17:17:43 +0100

    Update Transcriptase.java
    Fixed isStart() and added another method of generating proteins.

--- Java/src/io/github/qwert26/dieAgain/simulation/Transcriptase.java
+++ Java/src/io/github/qwert26/dieAgain/simulation/Transcriptase.java
@@ -29,13 +29,13 @@ public class Transcriptase {
 	 * @return
 	 */
 	public static final boolean isStart(char b1, char b2, char b3) {
-		if (b3 != 'g' || b3 != 'G') {
+		if (b3 != 'g' && b3 != 'G') {
 			return false;
 		}
-		if (b2 != 't' || b2 != 'T') {
+		if (b2 != 't' && b2 != 'T') {
 			return false;
 		}
-		return b1 != 'c' && b1 != 'C';
+		return !(b1 == 'c' || b1 == 'C');
 	}
 
 	/**
@@ -55,37 +55,30 @@ public class Transcriptase {
 		return v % 4 != 1;
 	}
 
-	public static void main(String[] args) {
-		Random source = new MiddleSquareSolo();
-		createProteins(1, source);
+	public static final int decode(char... cs) {
+		int ret = 0;
+		for (char c : cs) {
+			ret *= 4;
+			switch (c) {
+			case 't', 'T' -> ret += 0;
+			case 'c', 'C' -> ret += 1;
+			case 'a', 'A' -> ret += 2;
+			case 'g', 'G' -> ret += 3;
+			default -> ret /= 4;
+			}
+		}
+		return ret;
 	}
 
-	/**
-	 * 
-	 * @param count The number of proteins to create
-	 * @param r
-	 */
-	private static void createProteins(long count, Random r) {
-		List<String> proteins = new LinkedList<String>();
-		StringBuilder sb = null;
-		char last = '*';
-		long totalAminoAcids = 0;
-		for (; count > 0; count--) {
-			sb = new StringBuilder();
-			do {
-				last = CODON_TABLE_FLATTENED.charAt(r.nextInt(64));
-			} while (last == '*');
-			while (last != '*') {
-				sb.append(last);
-				totalAminoAcids++;
-				last = CODON_TABLE_FLATTENED.charAt(r.nextInt(64));
-			}
-			proteins.add(sb.toString());
-		}
+	public static void main(String[] args) {
+		Random source = new MiddleSquareSolo();
+		List<String> proteins = createStrandAndTranscribe(10000, source, false);
+		int totalAminoAcids = 0;
 		System.out.println("Individual Proteins by length:");
 		proteins.sort((s1, s2) -> s1.length() - s2.length());
 		for (String result : proteins) {
 			System.out.println(result.length() + "\t" + result);
+			totalAminoAcids += result.length();
 		}
 		System.out.println("Total amino acids: " + totalAminoAcids);
 		TreeMap<Character, Long> countByAminoAcid = new TreeMap<Character, Long>();
@@ -110,5 +103,81 @@ public class Transcriptase {
 			areaMismatch += Math.abs(relative - 1);
 		}
 		System.out.println("Absolute Area Mismatch: " + areaMismatch);
+	}
+
+	/**
+	 * 
+	 * @param count The number of proteins to create
+	 * @param r
+	 * @return A list of generated proteins.
+	 */
+	private static List<String> createProteins(long count, Random r) {
+		List<String> proteins = new LinkedList<String>();
+		StringBuilder sb = null;
+		char last = '*';
+		for (; count > 0; count--) {
+			sb = new StringBuilder();
+			do {
+				last = CODON_TABLE_FLATTENED.charAt(r.nextInt(64));
+			} while (last == '*');
+			while (last != '*') {
+				sb.append(last);
+				last = CODON_TABLE_FLATTENED.charAt(r.nextInt(64));
+			}
+			proteins.add(sb.toString());
+		}
+		return proteins;
+	}
+
+	/**
+	 * 
+	 * @param length            the length of the DNA-Strand to create
+	 * @param r
+	 * @param includeIncomplete If any incomplete proteins should be included in the
+	 *                          output: These proteins do not have a stop-codon on
+	 *                          the generated strand.
+	 * @return a list of transcribed proteins from a generated dna-strand.
+	 */
+	private static List<String> createStrandAndTranscribe(int length, Random r, boolean includeIncomplete) {
+		StringBuffer strand = new StringBuffer(length);
+		for (; length > 0; length--) {
+			switch (r.nextInt(4)) {
+			case 0 -> strand.append('T');
+			case 1 -> strand.append('C');
+			case 2 -> strand.append('A');
+			case 3 -> strand.append('G');
+			}
+		}
+		length = strand.length();
+		List<String> proteins = new LinkedList<String>();
+		for (int i = 2; i < length; i++) {
+			char first = strand.charAt(i - 2);
+			char second = strand.charAt(i - 1);
+			char third = strand.charAt(i);
+			if (isStart(first, second, third)) {
+				StringBuffer nextProtein = new StringBuffer(length / 3);
+				for (int current = i + 3; current < length; current += 3) {
+					char c1 = strand.charAt(current - 2);
+					char c2 = strand.charAt(current - 1);
+					char c3 = strand.charAt(current);
+					char decoded = CODON_TABLE_FLATTENED.charAt(decode(c1, c2, c3));
+					if (decoded == '*') {
+						if (!nextProtein.isEmpty()) {
+							proteins.add(nextProtein.toString());
+						}
+						nextProtein = null;
+						break;
+					} else {
+						nextProtein.append(decoded);
+					}
+				}
+				if (nextProtein != null && includeIncomplete) {
+					if (!nextProtein.isEmpty()) {
+						proteins.add(nextProtein.toString());
+					}
+				}
+			}
+		}
+		return proteins;
 	}
 }
